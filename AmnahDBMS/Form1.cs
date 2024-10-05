6fc7d7d7b1ac756b9270dbaf0f03bd90845be61e
Amnahmumtaz97 <iamnah97@gmail.com>
2024-10-05 17:07:08 +0500

    Add project files.

---
+++ AmnahDBMS/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AmnahDBMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = "Data Source=AMNAH;Initial Catalog=project;Integrated Security=True;Trust Server Certificate=True";
+           //
+           SqlConnection sql= new SqlConnection(ConnectionString);
+            //
+            sql.Open();
+
+        }
+
+    }
+}
